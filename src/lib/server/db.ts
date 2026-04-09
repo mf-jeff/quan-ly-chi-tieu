6a13100b908af6673ed37e35847b2228f8fea8c7
mf-jeff <mf-jeff@users.noreply.github.com>
2026-04-10 00:22:08 +0700

    fix: pin @prisma/adapter-libsql to v6 to match @prisma/client v6

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -7,13 +7,13 @@ function createPrismaClient() {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     const { createClient } = require("@libsql/client");
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaLibSql } = require("@prisma/adapter-libsql");
+    const { PrismaLibSQL } = require("@prisma/adapter-libsql");
     const libsql = createClient({
       url: process.env.TURSO_DATABASE_URL,
       authToken: process.env.TURSO_AUTH_TOKEN,
     });
-    const adapter = new PrismaLibSql(libsql);
-    return new PrismaClient({ adapter } as never);
+    const adapter = new PrismaLibSQL(libsql);
+    return new PrismaClient({ adapter });
   }
   return new PrismaClient();
 }
